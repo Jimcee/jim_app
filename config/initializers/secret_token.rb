4c72ee186fe768c4c83aaf0474c929e735d07c5e
Jim Colgan <jim.colgan@gmail.com>
2012-02-12 20:12:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JimApp::Application.config.secret_token = 'd8d11dd272b21de160e02856f1b0ac8374d260323c4a2400dea106d4fc1744e789a5d7e5ba61fad828dae91eb238d14d19c97ad24b89dc9bbdd076bf497a1bc0'
